0307546b249183ddc422f3aa41c5d3fa74c14448
sanji-bots-8256 <zoolzde773@cortinas.homes>
2024-06-28 22:47:35 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ 𝐃ᴇᴠᴇʟᴏᴘᴇᴅ 𝐁ʏ : <a href='https://t.me/Straw_Hat_Bots'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ ꭙ 𝐁ᴏᴛs</a>\n○ 𝐁ᴏᴛ 𝐔ᴘᴅᴀᴛᴇs : <a href='https://t.me/Straw_Hat_Bots'>𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ</a>\n○ 𝐁ᴏᴛ 𝐒ᴜᴘᴘᴏʀᴛ : <a href='https://t.me/Straw_hat_support'>𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀᴛ : <a href='https://t.me/+BSL2tOp6BGE2MGU1'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ 𝐆ʀᴏᴜᴘ </a></b>",
+            text = f"<b>○ 𝐃ᴇᴠᴇʟᴏᴘᴇᴅ 𝐁ʏ : <a href='https://t.me/Straw_Hat_Bots'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ ꭙ 𝐁ᴏᴛs</a>\n○ 𝐁ᴏᴛ 𝐔ᴘᴅᴀᴛᴇs : <a href='https://t.me/Straw_Hat_Bots'>𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ</a>\n○ 𝐁ᴏᴛ 𝐒ᴜᴘᴘᴏʀᴛ : <a href='https://t.me/Straw_hat_support'>𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀᴛ : <a href='https://t.me/+BSL2tOp6BGE2MGU1'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ ꭙ 𝐆ʀᴏᴜᴘ </a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
